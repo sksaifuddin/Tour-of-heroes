5419c111995aadcac4799ad53e2f991cb9152bf8
shaik saifuddin <atushaik@gmail.com>
2017-12-10 15:31:25 +0530

    first commit

---
+++ app/heroes/heroes.component.ts
@@ -0,0 +1,27 @@
+import { HEROES } from './../mock-heroes';
+import { Hero } from './../hero';
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+
+heroes = HEROES;
+
+
+selectedHero: Hero;
+
+
+onSelect(hero: Hero) {
+  this.selectedHero = hero;
+}
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
