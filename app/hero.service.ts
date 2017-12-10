c2477f4eb279c0b3d64dae0f56c5d7811bd39f27
shaik saifuddin <atushaik@gmail.com>
2017-12-10 22:42:51 +0530

    4th commit

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,18 +1,22 @@
+import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
 import { Injectable } from '@angular/core';
 import {Observable} from 'rxjs/observable';
 import {of} from 'rxjs/observable/of';
 
+
 @Injectable()
 export class HeroService {
 
 
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService:fetched data');
     return of(HEROES);
   }
 
+  constructor(private messageService: MessageService ) { }
 
-  constructor() { }
+
 
 }
